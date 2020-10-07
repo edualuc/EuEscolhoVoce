30768e7ad41c213f7cbfcb67ab74b7e24e8016b0
@edualuc Eduardo A Lucas <edualuc@gmail.com>
2020-10-07 01:46:08 -0300

    Pagina de escolhe de tipo de pokemon

--- components/ChoiceType/ChoiceType.js
+++ components/ChoiceType/ChoiceType.js
@@ -1,18 +1,32 @@
 import Link from 'next/link'
+import styled from 'styled-components'
 
-const styles = {
-  title: undefined
-}
+const Types = styled.ul`
+  display: flex;
+  justify-content: center;
+  align-content: center;
+`
+const Type = styled.li`
+  width: 30%;
+  text-align: center;
+  font-size: 2rem;
+  margin: 50px;
+`
+const H3 = styled.h3`
+  text-align: center;
+  font-size: 1.4rem;
+  margin: 0;
+`
 
 const ChoiceType = ({title, types}) => (
   <>
-    <h1 className={styles.title}>
+    <H3>
       {title}
-    </h1>
+    </H3>
 
-    <ul>
-      {types && types.map(type => <li key={type.name}><Link href={type.link}>{type.name}</Link></li>)}
-    </ul>
+    <Types>
+      {types && types.map(type => <Type key={type.name}><Link href={type.path}>{type.name}</Link></Type>)}
+    </Types>
   </>
 )
 export default ChoiceType
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,35 @@
 import { useRouter } from 'next/router';
-import serviceTypes from '../services/serviceTypes'
+import styled from 'styled-components'
 
+import serviceTypes from '../services/serviceTypes'
 import ChoiceType from '../components/ChoiceType/ChoiceType'
 import Footer from '../components/Footer/Footer'
 import HtmlHeader from '../components/HtmlHeader/HtmlHeader'
 import styles from '../styles/Home.module.css'
+
+const Main = styled.main`
+  max-width: 800px;
+  width: 70%;
+  max-height: 400px;
+  height: 60%;
+  border: 3px double #AAA;
+`
+const H1 = styled.h1`
+  max-width: 800px;
+  width: 70%;
+  margin: 0;
+  font-size: 1.6rem;
+  font-weight: normal;
+  font-variant: small-caps;
+`
+const H2 = styled.h2`
+  max-width: 800px;
+  width: 70%;
+  margin: 0;
+  font-size: 1.4rem;
+  font-weight: lighter;
+  font-variant: small-caps;
+`
 
 export default function Home(props) {
 
@@ -18,11 +43,14 @@ export default function Home(props) {
 
   return (
     <div className={styles.container}>
-      <HtmlHeader title={title}/>
+      <HtmlHeader title={title} />
 
-      <main className={styles.main}>
-        <ChoiceType types={types} />
-      </main>
+      <H1>Eu Escolho Você!</H1>
+      <H2>Escolha seus pokemon para levar com você!</H2>
+
+      <Main className={styles.main}>
+        <ChoiceType title={title} types={types} />
+      </Main>
 
       <Footer />
     </div>
